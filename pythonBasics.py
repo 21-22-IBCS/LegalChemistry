e6cb0b84ba13458f9de15a856439c042a744ef6d
Kaiyue (Frank) Fan <kaiyue_fan@m018584.aw.org>
2021-09-21 15:26:09 -0700

    Frank fan cs program

---
+++ pythonBasics.py
@@ -0,0 +1,32 @@
+def main():
+
+    print("2pythonBasic.py")
+    print("9/21/2021")
+    #date and the file
+
+
+
+    number1 = 99
+    number2 = 98
+    print(number1 + number2)
+    #two integers plus together
+    print(number1 * number2)
+    #two integers mutiple together
+
+
+    print("There are currently_number of students in the USB")
+    student1 = 20
+    print("There are currently " + str(student1) + " " + "number of students in the USB")
+    student2 = 80
+    print("There are currently " + str(student2) + " " + "number of students in the USB")
+    USB = 114
+    print("There are currently " + str(USB) + " " +  "number of students in the USB")
+#number of students in the usb
+
+
+
+
+
+if __name__=="__main__":
+    main()
+    
